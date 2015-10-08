78cd7dea37b530be1c1275cf348ba834576a14c7
Eric Potvin <epotvin@axcient.com>
2015-10-08 09:41:37 -0700

    Added utility module

---
+++ Util.py
@@ -0,0 +1,12 @@
+"""Util Module"""
+
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    """Catch the CTRL+C signal"""
+    print '\nAborted: ' + str(sig) + str(frame)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
